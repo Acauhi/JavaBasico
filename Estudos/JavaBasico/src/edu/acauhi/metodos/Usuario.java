df1641bf261f7a42f2b0b65678595c08247517b6
Acauhi <marcio.acauhi@gmail.com>
2023-03-29 13:07:53 -0300

    Tipos Variaveis

---
+++ Estudos/JavaBasico/src/edu/acauhi/metodos/Usuario.java
@@ -0,0 +1,31 @@
+package edu.acauhi.metodos;
+
+import java.sql.SQLOutput;
+
+public class Usuario {
+
+    public static void main(String[] args) {
+
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+        System.out.println("TV ligada? " + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        SmartTv.mudarCanal(13);
+        System.out.println("Canal atualizado para: " + smartTv.canal);
+
+        smartTv.ligar();
+        System.out.println("Novo status -> TV Ligada? " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo status -> TV Ligada? " + smartTv.ligada);
+
+    }
+
+}
